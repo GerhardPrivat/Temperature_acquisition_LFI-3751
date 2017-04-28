a015daea25c4edeeafd2970a54d2a39a1ea1b60e
GerhardPrivat <Gerhard.Schunk@gmail.com>
2017-04-28 17:37:24 +0200

    Revert "added crosscorr option for trace 1 and trace 2"
    This reverts commit efad8172f01565b156817e57fd30b552b209d6cc.

--- Temperature_acquisition_LFI-3751/Temperature_acquisition_windows.py
+++ Temperature_acquisition_LFI-3751/Temperature_acquisition_windows.py
@@ -43,7 +43,7 @@ def readingwriting(timeint, countmax, newstdin, continuous, datalist, flag, sett
 
     #-- SERIAL CONNECTION --
     ser = serial.Serial(
-        port = "\\.\COM4",
+        port = "\\.\COM",
         baudrate = 19200,
         parity = serial.PARITY_NONE,
         stopbits = serial.STOPBITS_ONE,
